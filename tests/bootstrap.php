d1a08e7658108d802ae64f1c0742e1553a2ccd7f
Ilia Cheishvili <ilia.cheishvili@gmail.com>
2012-03-25 02:45:38 -0600

    tweak to file check

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL | E_STRICT);
 
 $autoload = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php';
-if (is_file($autoload)) {
+if (is_readable($autoload)) {
     require_once $autoload;
 } else {
     require_once $autoload . '.dist';
